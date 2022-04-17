2808dfdd974ea78a74edef9e786cd3b2b856a905
Luiz Bernardo <br.betech@gmail.com>
2022-04-17 14:50:11 -0300

    add eks terraform

---
+++ poc/main.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
+locals {
+  cluster_name = "brbernardo-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
